30b63051f88bd29fb0385021374386dc640a9a6d
Turnip Bill Saur <7314076@student.unpar.ac.idgit config --global user.name calvintorsa>
2018-08-22 00:51:39 +0700

    Initial Commit

---
+++ QuisApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuisApp
+//
+//  Created by Calvin Saragih on 22/08/18.
+//  Copyright © 2018 Calvin Saragih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
